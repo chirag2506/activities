2b94f1dfc6af02a9d1e3e4a826201011909e8628
Chirag <chiraggupta2506@gmail.com>
2022-02-25 17:13:29 +0530

    constructors

--- js_practice/constructor_functions/script_constructor.js
+++ js_practice/constructor_functions/script_constructor.js
@@ -1,7 +1,21 @@
 function Cat(name) {
     this.name = name;
     this.sound = "Meow";
-    }
+}
 
 let cat = new Cat("Tom");
 console.log(cat.sound);
+
+Cat.prototype.speak = function(){
+    console.log(this.sound);
+}
+cat.speak();
+Cat.prototype.getName = function(){
+    alert(this.name);
+}
+cat.getName();
+
+console.log(cat.constructor); //will return Cat function
+
+console.log(cat instanceof Cat); //true
+console.log(cat instanceof String); //false
